3acb42007b2d552d6748b5632b5e2bc88a175be5
Vivek Rana <vkrana.devx@gmail.com>
2026-06-15 23:30:29 +0530

    Basics of java

--- src/Basics/firstProgram.java
+++ src/Basics/firstProgram.java
@@ -2,6 +2,16 @@ package Basics;
 
 public class firstProgram {
     public static void main(String args[]){
-        System.out.println("Hello World");
+        System.out.println("Hello World , My first Program");
+//        different types of data types in java
+        byte b = 125;
+        int a = 123;
+        short d = 45;
+        long e = 123;
+
+//        floating numbers
+        double c = 12.5;
+        float f = 12.7f;
+
     }
 }
