77b5da01bb745a6621db223b95b28421275f4e94
Vivek Rana <vkrana.devx@gmail.com>
2026-06-16 23:44:49 +0530

    HeronsFormula

---
+++ src/Basics/HeronsFormula.java
@@ -0,0 +1,15 @@
+package Basics;
+import java.util.Scanner;
+public class HeronsFormula {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        double s = (a+b+c)/2;
+        double area = Math.sqrt((s*(s-a)*(s-b)*(s-c)));
+        System.out.printf("%.2f",area);
+
+
+    }
+}
