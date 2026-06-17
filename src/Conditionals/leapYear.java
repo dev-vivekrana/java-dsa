f5621e3bc38025710e34d3c3b7cc9634af12e21e
Vivek Rana <vkrana.devx@gmail.com>
2026-06-17 13:11:55 +0530

    leap Year

--- src/Conditionals/leapYear.java
+++ src/Conditionals/leapYear.java
@@ -5,7 +5,12 @@ public class leapYear {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the year");
         int year = sc.nextInt();
-        if( (year % 4 ==0) || (year % 100 == 0 && year % 400 == 0)) System.out.println(year + " is a leap year");
+//         leap year conditions
+//        1. year must be multiple of 400
+//        OR
+//        2. if a year  is divisible by 4 then should not be divisible by 100
+
+        if( (year % 4 ==0 && year % 100 != 0) || (year % 400 == 0)) System.out.println(year + " is a leap year");
         else System.out.println("Not a leap year");
     }
 }
