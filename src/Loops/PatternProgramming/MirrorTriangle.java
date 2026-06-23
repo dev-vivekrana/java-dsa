cf6df7bd38d7cab250da90e156b7807c2a8388b3
Vivek Rana <vkrana.devx@gmail.com>
2026-06-23 14:30:54 +0530

    Mirror

---
+++ src/Loops/PatternProgramming/MirrorTriangle.java
@@ -0,0 +1,23 @@
+package Loops.PatternProgramming;
+
+import java.util.Scanner;
+
+public class MirrorTriangle {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+//            for spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print("   ");
+            }
+//            for stars
+            for(int k=1;k<=i;k++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+    }
+}
