457dad889279dcb6dd8ca32d1c38d47972042ac3
Vivek Rana <vkrana.devx@gmail.com>
2026-06-28 21:51:39 +0530

    Natural Number

--- src/Loops/naturalNumber.java
+++ src/Loops/naturalNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class naturalNumber {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the natural number");
+        System.out.println("Enter the natural number : ");
         int n = sc.nextInt();
         for(int i = 0;i<n;i++){
             System.out.print(i+1 + " ");
