64c2641aa02961a3c18949c3c2e09442dcb8cb57
Vivek Rana <vkrana.devx@gmail.com>
2026-07-25 23:23:53 +0530

    negative left and positive right using 2 pointer

---
+++ src/Array/sort.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sort {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {0,2,-3,4,0};
+        int i = 0;
+        int j = 0;
+        while(i< arr.length){
+//           swap the values
+            if(arr[i]<0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+            i++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
