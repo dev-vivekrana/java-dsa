c8361118bd4c0d521937a25da5dddf6c15a30cb3
Vivek Rana <vkrana.devx@gmail.com>
2026-07-25 21:08:32 +0530

    left rotate array by k

--- src/Array/leftRotateByK.java
+++ src/Array/leftRotateByK.java
@@ -8,18 +8,16 @@ public class leftRotateByK {
         Scanner sc = new Scanner(System.in);
         int[] arr = {1,2,3,4,5};
         System.out.println("Enter number by which u want to left rotate array : ");
-        int count = 0;
         int number = sc.nextInt();
-        for(int j=0;j<(number)%(arr.length);j++){
+        number = (number)%(arr.length);
+        for(int j=0;j<number;j++){
             int temp = arr[0];
             for(int i=0;i<arr.length-1;i++){
                 arr[i] = arr[i+1];
-                count++;
             }
             arr[arr.length-1] = temp;
         }
         System.out.println(Arrays.toString(arr));
-        System.out.println(count);
 
 
     }
