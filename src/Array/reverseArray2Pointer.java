6392390e08207a4b6e161dabe1a2cbbc3ddd5ed8
Vivek Rana <vkrana.devx@gmail.com>
2026-07-25 22:13:43 +0530

    reverse array 2 pointer

---
+++ src/Array/reverseArray2Pointer.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class reverseArray2Pointer {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {1,2,3,4,5};
+        int i = 0;
+        int j = arr.length-1;
+        int count = 0;
+        while(i<j){
+//           swap the values
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+            count++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
