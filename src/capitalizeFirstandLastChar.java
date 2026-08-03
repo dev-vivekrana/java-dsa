f6eccc2bfa0dcdbcc259c539ed01f9fd2e7460f1
Vivek Rana <vkrana.devx@gmail.com>
2026-08-04 01:44:19 +0530

    capitalize First andLast Character

---
+++ src/capitalizeFirstandLastChar.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class capitalizeFirstandLastChar {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String : ");
+        String s = sc.nextLine();
+        String ans = "";
+        // first convert the string into the array so we will split it;
+        String[] str = s.split(" ");
+//        Now use enhanced for loop to iterate over each word;
+        for(String word :str){
+            if(word.length()<=2) ans = ans + word.toUpperCase() + " ";
+            else ans = ans + Character.toUpperCase(word.charAt(0)) + word.substring(1, word.length()-1) + Character.toUpperCase(word.charAt(word.length()-1)) + " ";
+        }
+        System.out.println(ans);
+    }
+}
